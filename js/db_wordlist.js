465fe683603ee09b7b5a784286806cc51ef0b61b
Asher Lien <birdym.tw@gmail.com>
2023-03-23 14:32:43 +0800

    db_wordlist重構(20% )_完成顯示列表功能

--- js/db_wordlist.js
+++ js/db_wordlist.js
@@ -6,4 +6,30 @@ const filterRows = document.querySelectorAll('.filter-row');
 // ---抓取資料並拆解---
 const acc = JSON.parse(localStorage.getItem('userData'));
 const { ownLibrary } = acc;
+
 window.addEventListener('DOMContentLoaded', loadList);
+
+function loadList() {
+  ownLibrary.forEach((_, i) => {
+    const { chName, engName, level } = ownLibrary[i];
+    console.log(chName, engName, level);
+    const levelIcon = lv => {
+      let output = [];
+      for (let i = 0; i < level; i++) {
+        output.push('<img class="icon-s" src="svg/02_icon/icon-star.svg" />');
+      }
+      return output.join('');
+    };
+    //Html:
+    let temp = `<div class="filter-row">
+        <div class="word">${engName}</div>
+        <div class="word">${chName}</div>
+        <div class="word">
+          <div class="starbox"> ${levelIcon(level)}</div>
+          <a href="#" class="icon-s icon-delete">
+            <img src="svg/02_icon/icon-wrong_grey.svg" data-index="${i}" />
+          </a>
+        </div>`;
+    output.insertAdjacentHTML('beforeend', temp);
+  });
+}
